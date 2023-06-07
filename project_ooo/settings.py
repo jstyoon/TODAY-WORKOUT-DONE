55ee6224c389039599e1c0838d8799fb4c0218e5
DESKTOP-I3AF88B\USER <lys11556633@gmail.com>
2023-06-07 13:51:23 +0900

    :art:Change app name
    app name이 파스칼 케이스 사용하는 클래스명과 중복
    app name 수정, ERD 변경

--- project_ooo/settings.py
+++ project_ooo/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'User',
-    'Article',
+    
+    'users',
+    'articles',
 
 # drf 위치
     'rest_framework',
