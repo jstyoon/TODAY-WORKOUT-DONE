5208f6a094d876dd5da8afc8dd2cd7279506d242
na <lmh9205@gmail.com>
2023-06-07 15:45:38 +0900

    :sparkles:Feat: 댓글기능 1차 추가

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from articles.models import Article, Comment
+
+admin.site.register(Article)
+admin.site.register(Comment)
+
 # Register your models here.
--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
 
 # Create your models here.
+
+
+class User(AbstractBaseUser):
+
+    email = models.EmailField(
+        max_length=50, unique=True, null=False, blank=False)
+    password = models.CharField(max_length=200)
+    avatar = models.ImageField(blank=True)
+    bio = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
