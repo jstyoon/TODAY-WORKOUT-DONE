d49a883514f144147a30ac5bbd9a86fef9b887aa
sojin park <psjin977@naver.com>
2023-06-07 15:35:54 +0900

    :books: Docs: app.py fix

--- Article/apps.py
+++ Article/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ArticleConfig(AppConfig):
+class articleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Article'
+    name = 'article'
