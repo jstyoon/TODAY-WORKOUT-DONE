83ddfeeb969b37b69e7c13350cde5c182fe6493f
na <lmh9205@gmail.com>
2023-06-07 16:20:44 +0900

    임시

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from users.models import User, commonModel
 
 # Create your models here.
+
+
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=15)
+    content = models.CharField(max_length=150)
+    image = models.URLField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.TextField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
