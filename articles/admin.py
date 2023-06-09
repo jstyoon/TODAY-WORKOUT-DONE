20fcd79a9071a83fd357f38eb800815875a4174b
sojin park <psjin977@naver.com>
2023-06-09 21:17:26 +0900

    :art: article fix

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Articles
+from .models import Articles,Category,InSubCategory,OutSubCategory
 # Register your models here.
 
 admin.site.register(Articles)
+admin.site.register(Category)
+admin.site.register(InSubCategory)
+admin.site.register(OutSubCategory)
