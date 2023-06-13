6fe83f691eeded0785f7f00138708cf7f7c6e578
sojin park <psjin977@naver.com>
2023-06-14 01:27:08 +0900

    :art: article fix

--- articles/models.py
+++ articles/models.py
@@ -1,13 +1,5 @@
 from django.db import models
-<<<<<<< HEAD
-# from django.utils import timezone
-=======
-
-
-# Create your models here.
-
 from django.utils import timezone
->>>>>>> develop
 from users.models import User, commonModel
 # from django.db.models.signals import pre_save
 # from django.dispatch import receiver
@@ -84,18 +76,6 @@ class Articles(commonModel):
         return str(self.category)
 
 
-<<<<<<< HEAD
-
-
-
-
-=======
-@receiver(pre_save, sender=Articles)
-def update_complete_at(sender, instance, **kwargs):
-#   """ check_status값이 True값으로 전환될때 해당 시간을 저장함 """
-    if instance.check_status:
-        instance.complete_at = timezone.now()
->>>>>>> develop
 
 
 class Comment(commonModel):
