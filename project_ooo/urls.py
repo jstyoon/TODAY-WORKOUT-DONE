af7acf3d433470bbda0a2f9f3a1de01943cfd69a
na <lmh9205@gmail.com>
2023-06-16 22:31:49 +0900

    :memo: settings.y와 urls.py의  media 경로 재설정

--- project_ooo/urls.py
+++ project_ooo/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,6 @@ urlpatterns = [
     # allauth(외부인증)
     # path('users/', include('allauth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
