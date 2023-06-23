8b8bd469b5306e2d65d0d18815c0716fe9539312
DESKTOP-I3AF88B\USER <lys11556633@gmail.com>
2023-06-23 15:40:09 +0900

    :fire: :bulb:Del urlpatterns
    사용 안하는 url 주석 삭제

--- users/urls.py
+++ users/urls.py
@@ -8,20 +8,4 @@ urlpatterns = [
     path('', views.UserRegisterView.as_view()), 
     path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # dj-rest-auth/
-    # path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    # path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    
-    # path("verify-email/", VerifyEmailView.as_view(), name="rest_verify_email"),
-    # path("account-confirm-email/",VerifyEmailView.as_view(),name="account_confirm_email_sent",),
-    # path("account-confirm-email/<key>/",VerifyEmailView.as_view(),name="account_confirm_email",),
 ]
-
-""" dj-rest-auth
-    users/dj-rest-auth/registration/
-    users/dj-rest-auth/login/[name='rest_login']
-    users/dj-rest-auth/logout/[name='rest_logout']
-    users/dj-rest-auth/user/[name='rest_user_details']
-    users/dj-rest-auth/password/change/[name='rest_password_change']
-    users/dj-rest-auth/token/verify/[name='token_verify']
-    users/dj-rest-auth/token/refresh/[name='token_refresh'] """
