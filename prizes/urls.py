cda54639cc46d8b049249d6f2621d7199dd6eedf
DESKTOP-I3AF88B\USER <lys11556633@gmail.com>
2023-07-03 12:03:26 +0900

    for merge

--- prizes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.PrizeListAPIView.as_view(),name="prizes"),
-    path('<int:id>', views.PrizeDetailAPIView.as_view(),name="prize"),
-]
