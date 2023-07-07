a7e55596375391133cb8af7089f303bcafcbbdea
jstyoon <103176409+jstyoon@users.noreply.github.com>
2023-07-07 17:07:59 +0900

    Delete achievements directory

--- achievements/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Achievement
-
-
-class AchievementSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Achievement
-        fields = ['owner_id', 'date', 'description', 'amount', 'source']
