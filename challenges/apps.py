b454a720be94e062a9f7395e9124b0fa1c5b54ce
jstyoon <103176409+jstyoon@users.noreply.github.com>
2023-07-07 17:07:48 +0900

    Delete challenges directory

--- challenges/apps.py
+++
@@ -1,7 +0,0 @@
-""" docstring """
-from django.apps import AppConfig
-
-
-class ChallengesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'challenges'
--- challenges/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.ChallengeListAPIView.as_view(),name="challeges"),
-    path('<int:id>', views.ChallengeDetailAPIView.as_view(),name="challege"),
-]
